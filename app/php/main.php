2345dd943862804b79e54a5aeb7c43823390c1fd
Lapchenco Vitaly <lapchenco@gmail.com>
2015-01-19 16:27:25 +0400

    basic php structure

--- app/php/main.php
+++ app/php/main.php
@@ -2,20 +2,21 @@
 require_once ("core.php");
 /*----- условная структора основного скрипта -----*/
 
-if (isset($_SESSION)) {
-    if(/*поступил запрос на скачивание результата*/){
-        watermarkCreate();
-        fileDownload("$watermark"); // скачиваем водный знак
+//if (isset($_SESSION)) {
+    //if(/*поступил запрос на скачивание результата*/){
+        //watermarkCreate();
+        //fileDownload("$watermark"); // скачиваем водный знак
         //clean("tmp/$userDir");  // удаляем все временные файлы
-    }
+ //   }
 
 
-} else {
 
-    if ($_POST["files"]){
-        session_start();
-        $userDir = $_SESSION["ID"];
-        createTempDir("./tmp/$userDir"); //если сессии нет и есть запрос на загрузку файлов -> создаём временную дирректорию
-        fileUpload($_POST["files"]);//загружаем файлы
-    }
-}
+//} else {
+
+//    if ($_POST["files"]){
+//        session_start();
+//        $userDir = $_SESSION["ID"];
+        //createTempDir("./tmp/$userDir"); //если сессии нет и есть запрос на загрузку файлов -> создаём временную дирректорию
+        //fileUpload($_POST["files"]);//загружаем файлы
+//    }
+//}
