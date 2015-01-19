5cece6aa27d1829a50c645abb7dae0929ffc06bd
Lapchenco Vitaly <lapchenco@gmail.com>
2015-01-19 16:27:25 +0400

    basic php structure

---
+++ app/php/core.php
@@ -0,0 +1,61 @@
+<?php
+require_once ("img-config.php");
+function getJs(){
+
+    $mainImage = "tmp/$userDir/main.jpg";//картинка фона
+    $stampImage = "tmp/$userDir/stamp.jpeg";//картинка водного знака
+    $watermark = "tmp/$userDir//watermark.png";//картинка после наложения
+
+
+    $stampPaddingLeft = $_SESSION["paddingLeft"];//отступ снизу
+    $stampPaddingTop = $_SESSION["paddingTop"];//отступ сверху
+    $stampOpacity = $_SESSION["opacity"]; // прозрачность в процентах
+    //todo записать в массив сессии данные из js
+}//в разработке
+
+function watermarkCreate($mainImage, $stampImage, $stampPaddingLeft, $stampPaddingTop, $stampOpacity){
+
+    //getJs();// берём данные из JS
+
+    $image = imagecreatefromjpeg($mainImage); /* todo  imagecreatefrom должна быть зависима от типа файлов*/
+    $stamp = imagecreatefromjpeg($stampImage);/* todo  imagecreatefrom должна быть зависима от типа файлов*/
+    $stampWidth = imagesx($stamp);
+    $stampHeight = imagesy($stamp);
+
+
+    imagecopymerge( $image, $stamp, $stampPaddingLeft, $stampPaddingTop, 0, 0, $stampWidth, $stampHeight, $stampOpacity);// Слияние штампа с фотографией.
+    imagepng($image, 'img/watermark.png');// Сохранение фотографии в файл
+    imagedestroy($image); //освобождение памяти
+} //создаёт водный знак (готово 80%)
+
+function createTempDir (){
+    //  todo    создание временной папки {создаёт дирректорию с именем сессии}
+}   //в разработке
+
+function fileUpload(){
+    //  todo    загрузка  файлов на сервер  во времнную папку {true if success}
+}       //в разработке
+
+function fileDownload(){
+//  todo    скачивание  файлов  с сервера из временной дирректории {}
+}     //в разработке
+
+function clean($dirName){
+    if (file_exists("./$dirName"))
+        foreach (glob("./$dirName/*") as $file)
+            unlink($file); // удаляет все файлы из дирректории
+
+    rmdir("$dirName");// удаляет дирректорию
+    session_destroy();// удаляет сессию
+}    //удаляет папку пользователя и его сессию (готово 90%)
+
+function resize($image, $newWidth, $newHeight) {
+    $image = imagecreatefromjpeg($image);/* todo  imagecreatefrom должна быть зависима от типа файлов*/
+    $oldWidth = imagesx($image);
+    $oldHeight = imagesy($image);
+    $newImage = imagecreatetruecolor($newWidth, $newHeight);
+    imagecopyresampled($newImage, $image, 0, 0, 0, 0, $newWidth, $newHeight, $oldWidth, $oldHeight);
+    $image = $newImage;
+    imagepng($image, 'img/resized.png');// Сохранение фотографии в файл
+    imagedestroy($image); //освобождение памяти
+}  //изменяет размер изображения (готово 80%)
