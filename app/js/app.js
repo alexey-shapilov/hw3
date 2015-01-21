b308c7ff324a2173d33fc5be0ce88b8c95ae566e
Vladimir Shevchuk <dosandkv@gmail.com>
2015-01-21 12:44:13 +0200

    add ability to change watermark position by using visual panel;

--- app/js/app.js
+++ app/js/app.js
@@ -8,7 +8,9 @@ var app = {
     addListeners: function() {
         var self = this;
 
+        self.downloadWatermarkImg();
         self.setWatermarkOpacity();
+        self.setWatermarkPositionByVisualPanel();
 
         console.log('addListeners');
     },
@@ -17,10 +19,101 @@ var app = {
 
     },
     downloadWatermarkImg: function() {
+        var self = this,
+            watermarkWrapper = $('.watermark-wrapper');
+
+        watermarkWrapper.css({
+            top: 0,
+            left: 0
+        });
+    },
+    setWatermarkPositionByVisualPanel: function() {
         var self = this;
 
+        $('.js-set-watermark-position').on('click', function(e) {
+            var targetElement = $(e.currentTarget),
+                watermarkWrapper = $('.watermark-wrapper'),
+                watermarkPosition = targetElement.data('watermarkPosition'),
+                positionBtn = targetElement.children('.box__cell-btn');
+
+            $('.box__cell-btn').not(positionBtn).removeClass('active');
+            positionBtn.addClass('active');
+
+            watermarkWrapper.removeAttr('style');
+            switch (watermarkPosition) {
+                case 'top-left':
+                    console.log(watermarkPosition);
+                    watermarkWrapper.css({
+                        top: 0,
+                        left: 0
+                    });
+                    break;
+                case 'top-middle':
+                    console.log(watermarkPosition);
+                    watermarkWrapper.css({
+                        top: 0,
+                        left: '50%',
+                        marginLeft: '-90px'
+                    });
+                    break;
+                case 'top-right':
+                    console.log(watermarkPosition);
+                    watermarkWrapper.css({
+                        top: 0,
+                        right: 0
+                    });
+                    break;
+                case 'center-left':
+                    console.log(watermarkPosition);
+                    watermarkWrapper.css({
+                        top: '50%',
+                        marginTop: '-90px'
+                    });
+                    break;
+                case 'center-middle':
+                    console.log(watermarkPosition);
+                    watermarkWrapper.css({
+                        top: '50%',
+                        marginTop: '-90px',
+                        left: '50%',
+                        marginLeft: '-90px'
+                    });
+                    break;
+                case 'center-right':
+                    console.log(watermarkPosition);
+                    watermarkWrapper.css({
+                        top: '50%',
+                        marginTop: '-90px',
+                        right: 0
+                    });
+                    break;
+                case 'bottom-left':
+                    console.log(watermarkPosition);
+                    watermarkWrapper.css({
+                        bottom: 0,
+                        left: 0,
+                    });
+                    break;
+                case 'bottom-middle':
+                    console.log(watermarkPosition);
+                    watermarkWrapper.css({
+                        bottom: 0,
+                        left:' 50%',
+                        marginLeft: '-90px'
+                    });
+                    break;
+                case 'bottom-right':
+                    console.log(watermarkPosition);
+                    watermarkWrapper.css({
+                        bottom: 0,
+                        right: 0
+                    });
+            }
+
+            console.log(watermarkPosition);
+        });
     },
-    setWatermarkPosition: function() {
+    setWatermarkPositionBySpinner: function() {
         var self = this;
 
     },
