b0d32baf9cf69fb713ac2b498feb555e3a7c3cd0
Vladimir Shevchuk <dosandkv@gmail.com>
2015-01-21 10:52:28 +0200

    add ability to change watermark opacity;

--- app/js/app.js
+++ app/js/app.js
@@ -1,18 +1,42 @@
-/*var app = {
-    initialize: function () {
+var app = {
+    initialize: function() {
         var self = this;
 
+        self.addListeners();
         console.log('application was initialized');
+    },
+    addListeners: function() {
+        var self = this;
+
+        self.setWatermarkOpacity();
+
+        console.log('addListeners');
+    },
+    downloadBackgroundImg: function() {
+        var self = this;
+
+    },
+    downloadWatermarkImg: function() {
+        var self = this;
+
+    },
+    setWatermarkPosition: function() {
+        var self = this;
+
+    },
+    setWatermarkOpacity: function() {
+        var self = this;
+
+        $('.ui-slider').slider({
+            slide: function (event, ui) {
+                $('.watermark-wrapper').css({opacity: 1 - ui.value / 100});
+            }
+        });
+    },
+    downloadImg: function() {
+        var self = this;
+
     }
 };
 
-app.initialize(); */
-
-$(function() {
-    $( ".slider" ).slider(
-            {
-                range: 'min',
-                max: 100,
-                min: 0
-    });
-});
+app.initialize();
